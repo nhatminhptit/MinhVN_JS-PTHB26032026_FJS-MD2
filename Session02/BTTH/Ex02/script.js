0faf1cb6386c9322db88372c4aa1d14e662a3d38
Minh <nhatminhptit405@gmail.com>
2026-05-16 00:43:38 +0700

    Hoan thien bai 02

---
+++ Session02/BTTH/Ex02/script.js
@@ -0,0 +1,6 @@
+const myUniversity = "Post and Telecommunication Institute of Technology";
+let studentName = "Minh";
+let studentAge = 20;
+let isProgrammingFun = false;
+
+console.log(myUniversity, studentName, studentAge, isProgrammingFun);
