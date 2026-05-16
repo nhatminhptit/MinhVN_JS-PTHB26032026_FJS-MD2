d1a99f44af3eda8b700892c188c5d727d8dd3d2a
Minh <nhatminhptit405@gmail.com>
2026-05-16 19:05:00 +0700

    Hoan thien bai 08

---
+++ Session02/BTVN/Ex08/script.js
@@ -0,0 +1,15 @@
+console.log("Giá trị của message trước khai báo:", message);
+
+var message = "Hello";
+
+console.log("Giá trị của message sau khai báo:", message);
+
+function test() {
+  var myName = "Nhat Minh";
+
+  console.log("Truy cập trong hàm:", myName);
+}
+
+test();
+
+console.log("Truy cập từ ngoài hàm:", myName);
