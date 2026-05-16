812e2d3365484df2d2f6a52bad16aadf48572b7f
Minh <nhatminhptit405@gmail.com>
2026-05-16 09:39:29 +0700

    Hoan thien bai 05

---
+++ Session02/BTVN/Ex05/script.js
@@ -0,0 +1,8 @@
+let numberA = 5;
+let stringB = "5";
+
+console.log(numberA + " == " + stringB, numberA == stringB);
+console.log(numberA + " === " + stringB, numberA === stringB);
+
+let isUnderstood = confirm("Bạn đã hiểu bài chưa?");
+console.log("Bạn đã hiểu bài chưa?", isUnderstood);
